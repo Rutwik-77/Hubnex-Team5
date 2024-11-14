3b9de7148612030dc7ffb7b58fad03b9774125d4
Rutwik-77 <rutwiksawarkar7777@gmail.com>
2024-11-14 22:23:50 +0530

    First Commit PartnerPage

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import Header from "./Component/Header/Header";
+import Footer from "./Component/Footer/Footer";
+import Partner from "./pages/Partner";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    
+      <Partner></Partner>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
